5d2f6d7e87b4170c6bb20d98839d35809583896f
daianacapita <capitadaiana@gmail.com>
2025-12-02 11:06:02 +0100

    Atualizacao do app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import os
 
+def create_app(test_config=None):
+    # Vamos criar a configuracao do app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "app.sqlite")
+    )
 
+    if test_config is None:
+        # Carregar a configuracao do arquivo instance, se existir
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Carregar a configuracao de teste
+        app.config.from_mapping(test_config)
 
-def create_app():
-    app = Flask(__name__)       
-    @app.route('/')
-    def hello_world():
-        return "Hello, World!"
-    
-    return app
+    # Garantir que o diretório instance existe
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/pagina-inicial")
+    def pagina_inicial():
+        return "Bem-vindo ao aplicativo Flask!"
